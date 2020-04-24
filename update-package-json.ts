6c9f57858cf7a918238282cd7abcc72758ef9818
Dylan Frankland <dfrankland@users.noreply.github.com>
2020-04-24 15:27:00 -0700

    Fix the weird package.json created by wasm-pack

--- update-package-json.ts
+++ update-package-json.ts
@@ -1,6 +1,7 @@
 import { writeFileSync } from 'fs';
 import { resolve as resolvePath } from 'path';
 import glob from 'glob';
+import mainPackageJson from './package.json';
 import packageJson from './pkg/package.json';
 
 const pkgDir = resolvePath(__dirname, 'pkg');
@@ -8,6 +9,10 @@ const pkgDir = resolvePath(__dirname, 'pkg');
 packageJson.browser = 'node_jsxn_browser.js';
 
 packageJson.files = glob.sync('*.+(js|ts|wasm)', { cwd: pkgDir });
+
+packageJson.repository = mainPackageJson.repository;
+packageJson.bugs = mainPackageJson.bugs;
+packageJson.homepage = mainPackageJson.homepage;
 
 writeFileSync(
   resolvePath(pkgDir, 'package.json'),
